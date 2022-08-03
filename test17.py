38f2df6692700de31abe3f9cfb3b65173c8ce5b9
xiaozhi <lzhi14764@gmail.com>
2022-08-03 22:08:01 +0800

    test

--- test17.py
+++ test17.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import string
-
-s = raw_input('请输入一个字符串:\n')
+s = raw_input('请输入字符:\n')
 letters = 0
 space = 0
 digit = 0
@@ -20,4 +18,4 @@ while i < len(s):
         digit += 1
     else:
         others += 1
-print 'char = %d,space = %d,digit = %d,others = %d' % (letters, space, digit, others)
+print 'char = %d,space = %d,digit = %d,others = %d'%(letters,space,digit,others)
