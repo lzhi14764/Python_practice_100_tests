743674f6022f6e209501764320719649f1349e5a
xiaozhi <lzhi14764@gmail.com>
2022-08-07 00:03:38 +0800

    test

--- test35.py
+++ test35.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-def hello_runoob():
-    print 'runoob'
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-def hello_runoobs():
-    for i in range(3):
-        hello_runoob()
-if __name__ == '__main__':
-    hello_runoobs()
+
+print (bcolors.WARNING + "警告的颜色字体?" + bcolors.ENDC)
