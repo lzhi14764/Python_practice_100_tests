6e8d8e8df26e3aa13415fb88b5e451f28fa25957
xiaozhi <lzhi14764@gmail.com>
2022-08-12 23:20:35 +0800

    test

---
+++ test48.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+if __name__ == '__main__':
+    a = 1
+    b = 20
+if a > b:
+    print '%d 大于 %d'%(a,b)
+elif a == b:
+    print '%d 等于 %d'%(a,b)
+elif a < b:
+    print '%d 小于 %d'%(a,b)
+else:
+    '其他'
